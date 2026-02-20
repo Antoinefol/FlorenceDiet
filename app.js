85e3433f6e1a7abf88edf124e91bd0c2b19f6665
Antoinefol <antfoliard@gmail.com>
2026-02-20 22:19:51 +0100

    maquette terminée

--- app.js
+++ app.js
@@ -18,3 +18,49 @@ const observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll("[class*='reveal-']").forEach(function (r) {
   observer.observe(r);
 });
+
+const wrap = document.querySelector(".wrap-avis");
+const avis = document.querySelectorAll(".avis");
+const carousel = document.querySelector(".carousel");
+
+let index = 0;
+
+function updateCarousel() {
+
+    const slideWidth = avis[0].offsetWidth;
+    const gap = 50;
+
+    const carouselWidth = carousel.offsetWidth;
+    const wrapWidth = wrap.scrollWidth;
+
+    // position normale
+    let translateX = (carouselWidth / 2) - (slideWidth / 2) - index * (slideWidth + gap);
+
+    // limites pour empêcher le vide
+    const maxTranslate = 0;
+    const minTranslate = carouselWidth - wrapWidth;
+
+    if (translateX > maxTranslate) translateX = maxTranslate;
+    if (translateX < minTranslate) translateX = minTranslate;
+
+    wrap.style.transform = `translateX(${translateX}px)`;
+}
+
+function nextSlide() {
+
+    index++;
+
+    if (index >= avis.length)
+        index = 0;
+
+    updateCarousel();
+}
+
+// initialisation
+updateCarousel();
+
+// responsive
+window.addEventListener("resize", updateCarousel);
+
+// auto slide
+setInterval(nextSlide, 3000);
